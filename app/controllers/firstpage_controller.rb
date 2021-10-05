14b3c757e9ca6f8806eeb72f589372b92a0a3f75
chanonab <91712136+chanonab@users.noreply.github.com>
2021-10-05 16:55:10 +0700

    binding data from controller

--- app/controllers/firstpage_controller.rb
+++ app/controllers/firstpage_controller.rb
@@ -1,5 +1,7 @@
 class FirstpageController < ApplicationController
   def index
     @test_data = "Hi Ruby"
+    @sample_data = ('A'..'Z').to_a.sample(5)
+    @img_data = "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v535batch2-mynt-43.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=9f602de67a347b7c50ef8eeac3835189"
   end
 end
